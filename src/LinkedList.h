11f3038f4372657a10d4e41ea6c9f46e36120eaa
Ausha Homer <Aushahomer@weber.mail.edu>
2025-07-09 20:38:51 -0600

    fin 2nd video, constructor and destructor.

--- src/LinkedList.h
+++ src/LinkedList.h
@@ -13,28 +13,49 @@ struct Node
 };
 
 template <class Type>
-class LinkedList
+class LinkedList;
+
+template <class Type>
 std::ostream& operator<<(std::ostream& out, const LinkedList<Type>& list);
 
 template <class Type>
 class LinkedList {
     
     public:
-    
-    LinkedList();// Default constructor for the LinkedList.  Creates an empty list by setting front and back of the list to nulllptr
-    ~LinkedList(); //Destructor for the LinkedList.  Creates an empty list by setting front and back of the list to nulllptr
-    void insert(Type data); //Adds to the back of the list
-    Type peek(int ndx); //Looks at an element at the given ndx value
-    void remove(int ndx); //Removes an element at the given ndx position
-    friend std::ostream& operator<< <>(std::ostream& out, const LinkedList<Type>& list); // Returns an output stream for displaying the LinkedList. Note, the declaration and definition may need an extra <> in there: std::ostream& operator<< <>(std::ostream& out, const LinkedList<Type>& list);
+        LinkedList();// Default constructor for the LinkedList.  Creates an empty list by setting front and back of the list to nulllptr
+        ~LinkedList(); //Destructor for the LinkedList.  Creates an empty list by setting front and back of the list to nulllptr
+        void insert(Type data); //Adds to the back of the list
+        Type peek(int ndx); //Looks at an element at the given ndx value
+        void remove(int ndx); //Removes an element at the given ndx position
+        friend std::ostream& operator<< <>(std::ostream& out, const LinkedList<Type>& list); // Returns an output stream for displaying the LinkedList. Note, the declaration and definition may need an extra <> in there: std::ostream& operator<< <>(std::ostream& out, const LinkedList<Type>& list);
     
     protected:
-    node <Type>* front;
-    node <Type>* back;
-    int count;
-
+        node <Type>* front;
+        node <Type>* back;
+        int count;
     
-    
-    ;}
+};
 
-#endif linkedlist_H
+    template <class Type>
+    LinkedList<Type>::LinkedList(){
+
+        front = nullptr;
+        back = nullptr;
+        count = 0;
+    };
+
+    template <class Type>
+    LinkedList<Type>::~LinkedList(){
+
+        Node <Type>* temp = front;
+        
+        while (front != null;)
+        {
+            temp = front;
+            front = front->next;
+            delete temp;
+            
+        }
+        
+    }
+#endif LINKEDLIST_H
