61d974c9c1a9a8ce0221e08b5e86423864618c7d
Ausha Homer <Aushahomer@weber.mail.edu>
2025-07-09 19:53:34 -0600

    setting up

---
+++ src/LinkedList.h
@@ -0,0 +1,20 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include<iostream>
+#include <sstream>
+
+
+
+class Linkedlist {
+    
+    public:
+
+
+    private:
+
+    
+    
+    ;}
+
+#endif linkedlist_H
